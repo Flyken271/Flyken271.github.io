255ad56cd0a9a90a37e83d428ffc60a07ba4093f
Flyken271 <jaredcollins99@gmail.com>
2020-09-09 00:53:09 -0400

    fixed homepage

---
+++ rant/precache-manifest.633b2fae17eb0deea5882bc87260122d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a3224ca62acdf9b7f403bf8ef28361",
+    "url": "/rant/index.html"
+  },
+  {
+    "revision": "2667dd96b8600e81d060",
+    "url": "/rant/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "89d22d7dca21de17ba25",
+    "url": "/rant/static/css/main.b5b222e3.chunk.css"
+  },
+  {
+    "revision": "2667dd96b8600e81d060",
+    "url": "/rant/static/js/2.bd421182.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rant/static/js/2.bd421182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d22d7dca21de17ba25",
+    "url": "/rant/static/js/main.8ab3c07f.chunk.js"
+  },
+  {
+    "revision": "667bd2a0b2d87788d98d",
+    "url": "/rant/static/js/runtime-main.3a3ffb80.js"
+  }
+]);
--- rant/service-worker.js
+++ rant/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.edf8cde66c6c884abf54c0092ae24d82.js"
+  "/rant/precache-manifest.633b2fae17eb0deea5882bc87260122d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rant/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

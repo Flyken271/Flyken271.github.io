f8356c6eac34e25e77e4747cca2c43ac2244ea19
Flyken271 <jaredcollins99@gmail.com>
2020-09-16 18:47:34 -0400

    Added Homebrewd

---
+++ homerbrewd/precache-manifest.60e5ff7f4a571e854825dfea6276712c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9697d4d167a5b790ed5207957e52d59a",
+    "url": "/rant/index.html"
+  },
+  {
+    "revision": "22b9b67a267b037f640b",
+    "url": "/rant/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1f5f52429a1da1c069fe",
+    "url": "/rant/static/css/main.194b864e.chunk.css"
+  },
+  {
+    "revision": "22b9b67a267b037f640b",
+    "url": "/rant/static/js/2.a8882e55.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rant/static/js/2.a8882e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5f52429a1da1c069fe",
+    "url": "/rant/static/js/main.ea4bcf73.chunk.js"
+  },
+  {
+    "revision": "486f3845526f1783d9c1",
+    "url": "/rant/static/js/runtime-main.d7461472.js"
+  }
+]);

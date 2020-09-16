eb7d62a8f772d60222fb35379d874552899018e0
Flyken271 <jaredcollins99@gmail.com>
2020-09-16 18:56:16 -0400

    Fixed Source

---
+++ homebrewd1/precache-manifest.fba6fd94e11c24c9781cee9a347e7525.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc33c5a027781c28f9e5e385ace9ce2",
+    "url": "/homebrewd1/index.html"
+  },
+  {
+    "revision": "22b9b67a267b037f640b",
+    "url": "/homebrewd1/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0c5d105171427139155f",
+    "url": "/homebrewd1/static/css/main.194b864e.chunk.css"
+  },
+  {
+    "revision": "22b9b67a267b037f640b",
+    "url": "/homebrewd1/static/js/2.a8882e55.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/homebrewd1/static/js/2.a8882e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c5d105171427139155f",
+    "url": "/homebrewd1/static/js/main.939bc28f.chunk.js"
+  },
+  {
+    "revision": "a48e8b8984b122f2c212",
+    "url": "/homebrewd1/static/js/runtime-main.c9b1bbc6.js"
+  }
+]);
--- homebrewd1/service-worker.js
+++ homebrewd1/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homebrewd/precache-manifest.3ccefb15990fc681e42536c45026541b.js"
+  "/homebrewd1/precache-manifest.fba6fd94e11c24c9781cee9a347e7525.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homebrewd/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homebrewd1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

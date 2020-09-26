14857e9c824c27bae738b618427c535b57ae8bcb
Flyken271 <jaredcollins99@gmail.com>
2020-09-25 21:55:44 -0400

    Changed theme; added new section below repo cards

--- src/pages/Showcase.js
+++ src/pages/Showcase.js
@@ -5,15 +5,8 @@ const Showcase = props => {
   return (
     <Jumbotron id="aboutme" fluid>
       <Container fluid>
-        <h1 className="display-3">About Me: </h1>
-        <p className="lead">
-          My name is Jared Collins, I've been programming since I was twelve
-          when my father introduced me into Visual Basic and Visual Studio. I've
-          ventured into many different domains including networking, building
-          computers, web development, game development and desktop development,
-          from this history of programming I've honed my skill into being a Full
-          stack web developer.
-        </p>
+        <h1 className="display-3">{props.title}</h1>
+        <p className="lead">{props.content}</p>
       </Container>
     </Jumbotron>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Row, Col } from "reactstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./global.css"
 import axios from "axios"
@@ -26,7 +27,15 @@ class Home extends React.Component {
     return (
       <>
         <Navbar />
-        <Showcase />
+        <Showcase
+          title="About me: "
+          content="My name is Jared Collins, I've been programming since I was twelve
+          when my father introduced me into Visual Basic and Visual Studio. I've
+          ventured into many different domains including networking, building
+          computers, web development, game development and desktop development,
+          from this history of programming I've honed my skill into being a Full
+          stack web developer."
+        />
         <div className="container">
           <div className="row">
             {this.state.repos.map((repo, index) => {
@@ -42,6 +51,38 @@ class Home extends React.Component {
             })}
           </div>
         </div>
+        <Row>
+          <Col>
+            <Showcase
+              title="Tool Belt: "
+              content="My toolbelt is filled with different technologies including but not limited to Node.JS, Strapi, CSS, HTML, JSON, React, Next.JS, Gatsby, Lua, JavaScript, Java, C#, C++ and C."
+            />
+          </Col>
+          <Col>
+            <img
+              id="showcaseCode"
+              alt="code"
+              src="https://specials-images.forbesimg.com/imageserve/5f302109ffad89f9130e07db/960x0.jpg"
+              width="600"
+              height="300"
+            ></img>
+          </Col>
+          <Col>
+            <img
+              id="showcaseImage"
+              alt="server"
+              src="https://www.serversuperstore.com/pub/media/catalog/product/cache/image/650x650/1d588632cf35348847454ff993d7ec54/r/7/r720_-_inside_1.jpg"
+              width="520"
+              height="400"
+            ></img>
+          </Col>
+          <Col id="secondCol">
+            <Showcase
+              title="Background: "
+              content="Some of my background spans from dedicated hardware to software development. I used to own, maintain and manage my own Dell R720 2U Rack-mount server for quite awhile, I ran ESXi on it with a main VM running Ubuntu Server, which then ran Apache 2 hosting my various websites. My web development really blossomed from there and consistantly grew then on."
+            />
+          </Col>
+        </Row>
       </>
     )
   }

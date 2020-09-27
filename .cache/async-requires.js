8d1daa50cf60f716fbb929b305fa5ad0f4a3a3b4
Flyken271 <jaredcollins99@gmail.com>
2020-09-26 22:58:39 -0400

    migrated to next.js

---
+++ .cache/async-requires.js
@@ -0,0 +1,15 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-challenge-js": () => import("./../../../src/pages/challenge.js" /* webpackChunkName: "component---src-pages-challenge-js" */),
+  "component---src-pages-ghrepo-js": () => import("./../../../src/pages/Ghrepo.js" /* webpackChunkName: "component---src-pages-ghrepo-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-navbar-js": () => import("./../../../src/pages/Navbar.js" /* webpackChunkName: "component---src-pages-navbar-js" */),
+  "component---src-pages-pdfdisplay-js": () => import("./../../../src/pages/pdfdisplay.js" /* webpackChunkName: "component---src-pages-pdfdisplay-js" */),
+  "component---src-pages-seo-js": () => import("./../../../src/pages/seo.js" /* webpackChunkName: "component---src-pages-seo-js" */),
+  "component---src-pages-showcase-js": () => import("./../../../src/pages/Showcase.js" /* webpackChunkName: "component---src-pages-showcase-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\jared\\Documents\\flyken.org\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\jared\\Documents\\flyken.org\\src\\pages\\404.js"))),
+  "component---src-pages-challenge-js": hot(preferDefault(require("C:\\Users\\jared\\Documents\\flyken.org\\src\\pages\\challenge.js"))),
+  "component---src-pages-ghrepo-js": hot(preferDefault(require("C:\\Users\\jared\\Documents\\flyken.org\\src\\pages\\Ghrepo.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\jared\\Documents\\flyken.org\\src\\pages\\index.js"))),
+  "component---src-pages-navbar-js": hot(preferDefault(require("C:\\Users\\jared\\Documents\\flyken.org\\src\\pages\\Navbar.js"))),
+  "component---src-pages-pdfdisplay-js": hot(preferDefault(require("C:\\Users\\jared\\Documents\\flyken.org\\src\\pages\\pdfdisplay.js"))),
+  "component---src-pages-seo-js": hot(preferDefault(require("C:\\Users\\jared\\Documents\\flyken.org\\src\\pages\\seo.js"))),
+  "component---src-pages-showcase-js": hot(preferDefault(require("C:\\Users\\jared\\Documents\\flyken.org\\src\\pages\\Showcase.js")))
+}
+

54ad88d5a5f1c0dd3d68257e3ba78a9fbffbda76
Flyken271 <jaredcollins99@gmail.com>
2020-09-27 11:59:30 -0400

    Changed colors and added theme to cards

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,19 @@ const Codeblock = styled.code`
   background: ${({ active }) => (active ? "#1d1d1d" : "#c4c4c4")};
   transition: background 0.9s ease;
 `;
+const Title = styled.h1`
+  & a {
+    color: ${({ active }) => (active ? "#eb4034" : "#346eeb")};
+  }
+`;
+const Card = styled.a`
+  &:hover,
+  &:focus,
+  &:active {
+    color: ${({ active }) => (active ? "#eb4034" : "#346eeb")};
+    border-color: ${({ active }) => (active ? "#eb4034" : "#346eeb")};
+  }
+`;
 
 export default function Home(repos) {
   const [active, setActive] = useState(false);
@@ -36,18 +49,22 @@ export default function Home(repos) {
           </Head>
 
           <main className={styles.main}>
-            <h1 className={styles.title}>
-              Welcome to my{" "}
-              <a href="https://github.com/Flyken271/Flyken271.github.io">
-                portfolio!
-              </a>
-            </h1>
+            <Title active={active}>
+              <h1 className={styles.title}>
+                Welcome to my{" "}
+                <a href="https://github.com/Flyken271/Flyken271.github.io">
+                  portfolio!
+                </a>
+              </h1>
+            </Title>
             <br />
-            <h1 className={styles.titleSub}>
-              <Link href="/about/">
-                <a>About me</a>
-              </Link>
-            </h1>
+            <Title active={active}>
+              <h1 className={styles.titleSub}>
+                <Link href="/about/">
+                  <a>About me</a>
+                </Link>
+              </h1>
+            </Title>
 
             <p className={styles.description}>
               I'm fluent in{" "}
@@ -61,20 +78,23 @@ export default function Home(repos) {
             <div className={styles.grid}>
               {repos.repos.map((repo, index) => {
                 return (
-                  <a key={index} href={repo.html_url} className={styles.card}>
-                    <h3>{repo.name} &rarr;</h3>
-                    <p>{repo.description}</p>
-                    <h5>{repo.full_name}</h5>
-                  </a>
+                  <Card key={index} active={active} className={styles.card}>
+                    <a href={repo.html_url}>
+                      <h3>{repo.name} &rarr;</h3>
+                      <p>{repo.description}</p>
+                      <h5>{repo.full_name}</h5>
+                    </a>
+                  </Card>
                 );
               })}
             </div>
 
             <br />
-
-            <h1 className={styles.titleSub}>
-              <a href="mailto:jaredcollins99@gmail.com">Hire Me</a>
-            </h1>
+            <Title active={active}>
+              <h1 className={styles.titleSub}>
+                <a href="mailto:jaredcollins99@gmail.com">Hire Me</a>
+              </h1>
+            </Title>
           </main>
 
           <footer className={styles.footer}>

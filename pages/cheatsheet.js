ac5fec299fb3885bac64396e1587c099df01d215
Flyken <39961800+Flyken271@users.noreply.github.com>
2020-12-26 12:23:23 -0500

    Added css cheatsheet

---
+++ pages/cheatsheet.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home(repos) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Cheatsheet</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+	  <embed src="CSS Selector Cheat Sheet - Dark.pdf" />
+    </div>
+  );
+}

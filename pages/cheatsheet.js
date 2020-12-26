07e79a19f173b2100732a72452b359e92285f3e0
Flyken <39961800+Flyken271@users.noreply.github.com>
2020-12-26 12:25:45 -0500

    Update cheatsheet.js

--- pages/cheatsheet.js
+++ pages/cheatsheet.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-export default function Home(repos) {
+export default function Cheatsheet(repos) {
   return (
     <div className={styles.container}>
       <Head>

493c9a42a5f2667396cec9e27e1e1e0a7fe9f8fd
Flyken <39961800+Flyken271@users.noreply.github.com>
2020-12-26 12:27:08 -0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import styled from "styled-components";
+import Cheatsheet from "./Cheatsheet";
 
 const Highlight = styled.span`
   color: #346eeb;
@@ -45,6 +46,8 @@ export default function Home(repos) {
           <Highlight>comfort</Highlight> for it was a new idea and journey
           adding to my past.
         </p>
+		<hr />
+		<Cheatsheet />
       </main>
 
       <footer className={styles.footer}>
